a3986f8ab0f2d255724726f965ef917b1d46cf6b
Alexander Ross <alex@aross.se>
2017-04-26 00:11:46 +0200

    First release

---
+++ config/deploy.rb
@@ -0,0 +1,29 @@
+lock "3.7.2"
+
+set :application, "todoapp2"
+set :repo_url, "git@github.com:alex-ross/todomvc-turbolinks.git"
+set :branch, "test2"
+set :deploy_to, "/usr/local/src/#{fetch :application}"
+
+namespace "docker" do
+  task "update" do
+    on roles(:app) do
+      within release_path do
+
+        docker_base = [
+          "docker-compose",
+          "-p", fetch(:application), 
+          "-f", "docker-compose.yml",
+          "-f", "config/docker-compose/production.yml"
+        ]
+
+        execute(*docker_base, "up", "-d", "--build", "--remove-orphans")
+        execute(*docker_base, "run", "--rm", "web", "bin/rails db:create", "2>&1")
+        execute(*docker_base, "run", "--rm", "web", "bin/rails db:migrate")
+      end
+    end
+  end
+end
+
+after "deploy:updating", "docker:update"
+
---
+++ config/deploy/production.rb
@@ -0,0 +1,1 @@
+server "aprilevent", roles: %w{app db web}
